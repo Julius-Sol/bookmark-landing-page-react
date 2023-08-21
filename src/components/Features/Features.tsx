44d49df3d4bfc30fb01f0ec6503aa77b9189cbeb
Julio S <85770548+Julius-Sol@users.noreply.github.com>
2023-08-21 19:00:12 -0400

    Finilize desktop design

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -1,7 +1,36 @@
 import '../../sass/components/Features.scss'
-import tab1Img from '../../assets/illustration-features-tab-1.svg'
+import featuresSlideData from '../../data/features-data';
+import { useState } from 'react'
 
 function Features() {
+
+  const[slideIndex, setSlideIndex] = useState(0);
+
+  const handleSlideSelection = (event:any) =>{
+    let res = document.querySelector('.selected');
+    res?.classList.remove('selected');
+    event.target.classList.add('selected');
+    let slideNumber = Number(event.target.attributes[1].value);
+    setSlideIndex(slideNumber);
+    
+  }
+
+  const slideArray = featuresSlideData.map((item,index)=>{
+    return(
+      <div className='features__slide-cont' key={index}>
+        <div className='features__image-container' >
+            <img className='features__image' src={item.img} alt="features image" />
+            <div className='features__image__blue-bg'></div>
+        </div>
+        <div className="features__slide">
+          <h2 className='features__slide__title'>{item.title}</h2>
+          <p className='features__slide__description'>{item.description}
+          </p>
+          <button className='features__button'>More Info</button>
+        </div>
+      </div>
+    )
+  });
   return (
     <section className='features'>
       <div>
@@ -10,24 +39,12 @@ function Features() {
           Your bookmarks sync between your devices so you can access them on the go.</p>
       </div>
     <div className='features__tab-list'>
-      <p className='features__tab-list__tab features__tab-list__tab--borders selected'>Simple Bookmarking</p>
-      <p className='features__tab-list__tab'>Speedy Searching</p>
-      <p className='features__tab-list__tab features__tab-list__tab--borders'>Easy Sharing</p>
+      <p className='features__tab-list__tab features__tab-list__tab--borders selected' data-slidenumber={0} onClick={handleSlideSelection}>Simple Bookmarking</p>
+      <p className='features__tab-list__tab' onClick={handleSlideSelection} data-slidenumber={1}>Speedy Searching</p>
+      <p className='features__tab-list__tab features__tab-list__tab--borders' data-slidenumber={2} onClick={handleSlideSelection}>Easy Sharing</p>
     </div>
 
-    <div className='features__slide-cont'>
-      <div className='features__image-container'>
-          <img className='features__image' src={tab1Img} alt="features image" />
-          <div className='features__image__blue-bg'></div>
-      </div>
-      <div className="features__slide">
-        <h2 className='features__slide__title'>Bookmark in one click</h2>
-        <p className='features__slide__description'>Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.
-        </p>
-
-        <button className='features__button'>More Info</button>
-      </div>
-    </div>
+    {slideArray[slideIndex]}
     </section>
   )
 }
---
+++ src/data/features-data.tsx
@@ -0,0 +1,24 @@
+import img1 from '../assets/illustration-features-tab-1.svg'
+import img2 from '../assets/illustration-features-tab-2.svg'
+import img3 from '../assets/illustration-features-tab-3.svg'
+
+const featuresArray =  [
+  {
+    img: img1,
+    title:'Bookmark in one click',
+    description:'Organize your bookmarks however you like. Our simple drag-and-drop interfacegives you complete control over how you manage your favourite sites.',
+
+  },
+  {
+    img: img2,
+    title:'Intelligent search',
+    description:'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.',
+  },
+  {
+    img: img3,
+    title:'Share your bookmarks',
+    description:' Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.',
+  },
+]
+
+export default featuresArray;
